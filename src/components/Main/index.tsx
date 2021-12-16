98cd6acf637e776eafb1dfbb2d0e10ad37ff2ae2
Guilherme Alves <guisouzanl@hotmail.com>
2021-12-16 05:14:52 -0300

    Boilerplate PWA

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,21 @@
-const Main = () => (
-  <main>
-    <h2>React Avançado</h2>
-  </main>
+import * as S from './styles'
+
+const Main = ({
+  title = 'React Avançado',
+  description = 'Typescript, ReactJS, NextJS'
+}) => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Desenvolvedor de frente para tela com um código"
+    />
+  </S.Wrapper>
 )
 
 export default Main
